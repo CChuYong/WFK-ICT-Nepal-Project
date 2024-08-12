ada5e6e2e5e88e79e8d2de4be09c4612f72f5053
ChuYong <jakgon@naver.com>
2024-08-12 14:33:49 +0545

    feat: add login logics

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,9 +1,17 @@
+"use client";
+import {useState} from "react";
+
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const doLogin = () => {
+        alert('hi');
+    }
     return (
         <div className="flex items-center justify-center min-h-screen">
             <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-xl">
                 <h2 className="text-2xl font-bold text-center text-gray-900">Login</h2>
-                <form className="space-y-6">
+                <div className="space-y-6">
                 <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                 ID
@@ -11,9 +19,11 @@ export default function Login() {
             <input
                 id="email"
                 name="email"
-                type="email"
+                type="text"
                 autoComplete="email"
                 required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
             </div>
@@ -27,6 +37,8 @@ export default function Login() {
                 type="password"
                 autoComplete="current-password"
                 required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
             </div>
@@ -50,13 +62,14 @@ export default function Login() {
             </div>
             <div>
             <button
-                type="submit"
+                type="button"
+                onClick={doLogin}
                 className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
         Login
             </button>
             </div>
-            </form>
+            </div>
         </div>
     </div>
     );
