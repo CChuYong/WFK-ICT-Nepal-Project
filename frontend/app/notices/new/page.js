4b35e44b962db4f20c7e15a888a4b2d7821bec73
ChuYong <jakgon@naver.com>
2024-08-13 14:19:38 +0545

    feat: fix typo

--- frontend/app/notices/new/page.js
+++ frontend/app/notices/new/page.js
@@ -46,7 +46,7 @@ export default function NewNotice() {
           />
         </div>
         <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-          Wirte
+          Write
         </button>
       </form>
     </div>
